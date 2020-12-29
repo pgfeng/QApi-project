2f9e0c575fb8d971655c8fb7525f95973f153172
潘高峰 <228375325@qq.com>
2020-12-29 15:43:45 +0800

    BUG fixed

--- App/Middleware/IndexMiddleware.php
+++ App/Middleware/IndexMiddleware.php
@@ -5,9 +5,9 @@ namespace App\Middleware;
 class IndexMiddleware extends \QApi\Http\MiddlewareHandler
 {
 
-    public function handle(\QApi\Request $request, \QApi\Response $response, \Closure $next): \QApi\Response
+    public function handle(\QApi\Request $request, \QApi\Response $response, \Closure $next): \QApi\Response|\Closure
     {
         $request->get['world'] = 'world';
-        return $next($request, $response);
+        return $next;
     }
 }
