5b82216d71490fd07190b6dfb3dd12c68da0dc70
潘高峰 <228375325@qq.com>
2021-05-12 10:21:17 +0800

    接口文档首次更新

--- App/Middleware/IndexMiddleware.php
+++ App/Middleware/IndexMiddleware.php
@@ -2,10 +2,14 @@
 
 namespace App\Middleware;
 
+use QApi\Attribute\Parameter\GetParam;
 use QApi\Http\MiddlewareInterface;
 
 class IndexMiddleware implements MiddlewareInterface
 {
+    #[
+        GetParam('world','文字描述','测试的文字描述!')
+    ]
     public function handle(\QApi\Request $request, \QApi\Response $response, \Closure $next): \QApi\Response|\Closure
     {
         $request->get['world'] = 'world';
--- App/V100/IndexController.php
+++ App/V100/IndexController.php
@@ -5,17 +5,21 @@ namespace App\V100;
 
 
 use App\Middleware\IndexMiddleware;
+use QApi\Attribute\Parameter\GetParam;
 use QApi\Attribute\Route;
 use QApi\Http\Request\MethodsEnum;
 use QApi\Request;
 use QApi\Response;
 
-#[Route(middleware: IndexMiddleware::class)]
+#[Route(middleware: IndexMiddleware::class, tag: 'hello')]
 class IndexController
 {
-    #[Route('/', methods: MethodsEnum::METHOD_GET)]
+    #[
+        Route('/', methods: MethodsEnum::METHOD_GET, summary: 'Hello world'),
+        GetParam('hello', '文字', '测试的描述信息', 'string', false, 'hello')
+    ]
     public function indexAction(Request $request, Response $response): Response
     {
-        return $response->setData('hello ' . $request->get['world'] . '!');
+        return $response->setData($request->get->get('hello') . ' ' . $request->get['world'] . '!');
     }
 }
